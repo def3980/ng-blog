02b3021484c3fe195198937788bbbc3f3d1a43ed
def3980 <def.3980@gmail.com>
2020-10-31 23:36:24 -0500

    Oswaldo: aplicando nuevas rutas
     - Creando nuevas rutas en app-routing
     - Modificando en nav-bar los links de acceso
       a los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: ArticleListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
