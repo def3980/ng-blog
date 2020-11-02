cba9cf86b2581c1a25f99536fd1f0ff20b7569c0
def3980 <def.3980@gmail.com>
2020-11-02 18:02:11 -0500

    Oswaldo: capturando parámetros
     - Se modifica el componente de articulo para recuperar
       el parámetro entregado por URL
     - Mostrando información capturada desde la URL en
       la vista de componente de artículo

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-
-  constructor() { }
+  article: Article = null;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.article = new Article();
+      this.article.key = params.key;
+    });
   }
-
 }
