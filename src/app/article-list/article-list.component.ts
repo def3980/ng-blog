3dcb6951ec1c2c99f4e20ede648c8bb65ba273d0
def3980 <def.3980@gmail.com>
2020-11-02 15:42:20 -0500

    Oswaldo:  mostrando lista de artículos
     - Creado clase de Artículos
     - Creado archivo de mocks para Artículos
     - Aplicando estilos para la lista de artículos
     - Mostrando los tags para la lista de artículos
     - Cargando la lista de mocks en el typescript

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../article';
+import { ARTICLES } from '../mock-articles';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.css']
+  styleUrls: ['./article-list.component.css'],
 })
 export class ArticleListComponent implements OnInit {
-
-  constructor() { }
+  articles: Article[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    this.articles = ARTICLES;
+    console.log('<=== this.articles: ===>', this.articles);
   }
-
 }
---
+++ src/app/mock-articles.ts
@@ -0,0 +1,23 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+  {
+    id: 1,
+    title: 'My first article',
+    content: '',
+    description: 'This is my first article! It\'s great. Please read it. :)',
+    key: 'my-first-article',
+    date: new Date(),
+    imageUrl: 'http://angular.io/assets/images/logos/angular/angular.svg',
+  },
+  {
+    id: 2,
+    title: 'The second article',
+    content: '',
+    description: 'Also a great artice',
+    key: 'the-second-article',
+    date: new Date(),
+    imageUrl:
+      'http://angular.io/assets/images/logos/angular/angular_solidBlack.svg',
+  },
+];
