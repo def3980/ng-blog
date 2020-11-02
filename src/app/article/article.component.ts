ef360f69e00a0127f097476b4a60bf25c857cdd9
def3980 <def.3980@gmail.com>
2020-11-02 18:26:40 -0500

    Oswaldo: validando rutas encontradas
     - Realizando validación en el componente de article
       para que devuelva información vacia en caso de no
       encontrar información del artículo

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Article } from '../article';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-article',
@@ -8,13 +9,22 @@ import { Article } from '../article';
   styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-  article: Article = null;
-  constructor(private route: ActivatedRoute) {}
+  article: Article = new Article();
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      this.article = new Article();
-      this.article.key = params.key;
+      const key = params.key;
+      this.articleService.getArticle(key).subscribe((article) => {
+        if (undefined === article) {
+          return;
+        }
+        this.article = article;
+        console.log('<=== this.article: ===>', this.article);
+      });
     });
   }
 }
